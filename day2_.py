c003d46130823265f34cacf8a56b83347517b4f8
DongDongJu <commisori28@gmail.com>
2015-08-27 01:17:46 +0900

    push homework

---
+++ day2_.py
@@ -0,0 +1,5 @@
+__author__ = 'DongJu'
+
+import sys
+
+print(sys.float_info.max)
---
+++ day3_socket_client.py
@@ -0,0 +1,46 @@
+
+import sys
+import socket
+import select
+
+def chat_client():
+    if(len(sys.argv) < 3) :
+        print('Usage : python3 chat_client.py hostname port')
+        sys.exit()
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    try :
+        s.connect((host, port))
+    except :
+        print('Unable to connect')
+        sys.exit()
+    print('Connected to remote host. You can start sending messages')
+    sys.stdout.write('[Me] '); sys.stdout.flush()
+    while 1:
+        socket_list = [sys.stdin, s]
+        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
+        for sock in ready_to_read:
+            if sock == s:
+                data = sock.recv(4096).decode("utf-8")
+                if not data :
+                    print('\nDisconnected from chat server')
+                    sys.exit()
+                else :
+                    sys.stdout.write(data)
+                    sys.stdout.write('you-> ')
+                    sys.stdout.flush()
+                    #print("여기1")
+            
+            else :
+                msg = sys.stdin.readline()
+                result = msg.encode("utf-8")
+                s.send(result)
+                sys.stdout.write('you-> ')
+                sys.stdout.flush()
+                #print("여기2")
+
+if __name__ == "__main__":
+
+    sys.exit(chat_client())
